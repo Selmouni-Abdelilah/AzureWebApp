03870710e0fec7ae6902e286cb4c9d7684207fbc
Selmouni-Abdelilah <128396251+Selmouni-Abdelilah@users.noreply.github.com>
2023-12-09 22:16:00 +0100

    Update providers.tf

--- Terraform/providers.tf
+++ Terraform/providers.tf
@@ -2,14 +2,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>2.0"
-    }
-    azuread = {
-      source = "hashicorp/azuread"
+      version = "~> 3.0.0"
     }
   }
+  required_version = ">= 0.14.9"
 }
-
 provider "azurerm" {
   features {}
 }
