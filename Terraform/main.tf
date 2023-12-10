4dd4b1ca7ff7b80f13aa0d9540947c8436cf4204
Selmouni-Abdelilah <128396251+Selmouni-Abdelilah@users.noreply.github.com>
2023-12-10 16:59:46 +0100

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -38,7 +38,7 @@ resource "azurerm_linux_web_app" "webapp" {
 #  Deploy code from a public GitHub repo
 resource "azurerm_app_service_source_control" "sourcecontrol" {
   app_id             = azurerm_linux_web_app.webapp.id
-  repo_url           = "https://github.com:Azure-Samples/app-service-web-nodejs-manage"
+  repo_url           = "https://github.com/Azure-Samples/nodejs-docs-hello-world"
   branch             = "master"
   use_manual_integration = true
   use_mercurial      = false
